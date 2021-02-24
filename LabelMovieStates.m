afa439d6efb3d6fff0f8e4b5006b6c970cd70e60
mikewehr <wehr@uoregon.edu>
2021-02-24 10:02:07 -0800

    Update LabelMovieStates.m
    resolving conflict

--- LabelMovieStates.m
+++ LabelMovieStates.m
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 % label trial videos with hmm states, and other info
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -149,6 +150,55 @@ for i=1:length(Groupdata);
      numframes=stop_frame-start_frame;
    % numframes=firstcontact_frame-start_frame; %mw 7.21.2020
 %numframes= firstcontact_frame;
+=======
+function LabelMovieStates(outputdir)
+
+%Usage: LabelMovieStates(datadir)
+% label trial videos with hmm states, and other info
+
+if nargin==0 outputdir=pwd;end
+cd(outputdir)
+if ~exist('./pruned_tpm.mat')
+    error('no smm group data in this directory')
+end
+load pruned_tpm
+load training_data
+
+
+out_movie_filename='labelled_trials';
+vout = VideoWriter(out_movie_filename, 'MPEG-4');
+vout.FrameRate=groupdata(1).framerate;
+open(vout)
+cumframeidx=0;
+cmap=jet(num_states);
+cmap=round(255*cmap);
+
+
+
+for i=1:length(datadirs);
+    % localframenum comes from ConvertGeometryToObservations and
+    % already accounts for trimming from cricket drop frame to catch
+    % frame
+    
+    start_frame=groupdata(i).cricketdropframe;
+    stop_frame=groupdata(i).catchframe;
+    
+  
+    datadir=datadirs{i};
+    if ismac
+        datadir= strrep(datadir, '\', '/');
+        datadir= strrep(datadir, 'D:', '/Volumes/wehrrig4.uoregon.edu');
+    end
+    
+    cd(datadir)
+    d=dir('*_labeled.mp4');
+    movie_filename=d.name;
+    fprintf('\nmovie %d/%d ', i,length(datadirs))
+    v = VideoReader(movie_filename);
+    oneframe = read(v, 1) ;
+    grey=mean(oneframe(:));
+    numframes=stop_frame-start_frame;
+>>>>>>> Stashed changes
     
     %add some labels
     x1=10; y1=10;
@@ -161,6 +211,7 @@ for i=1:length(Groupdata);
         cumframeidx=cumframeidx+1;
         vidFrame = read(v, start_frame+f-1) ;
         
+<<<<<<< Updated upstream
  
 
 
@@ -198,24 +249,51 @@ for i=1:length(Groupdata);
         vidFrame = insertText(vidFrame,[x1,y1],str,...
             'FontSize',60,'Font', 'Arial', 'BoxColor', boxcolor,  ...
             'BoxOpacity',0.4,'TextColor','white');
+=======
+       
+        
+        % add state label
+        if survival_mask(cumframeidx) %surviving epoch
+            boxcolor=cmap(1+Z(cumframeidx),:);
+            textcolor='white';
+        else %this epoch was pruned away
+            boxcolor='black';
+            textcolor=[50 50 50];
+        end
+        
+        str=sprintf('%d', Z(cumframeidx));
+        vidFrame = insertText(vidFrame,[x1,y1],str,...
+            'FontSize',60,'Font', 'Arial', 'BoxColor', boxcolor,  ...
+            'BoxOpacity',0.4,'TextColor',textcolor);
+>>>>>>> Stashed changes
         %         str=sprintf('%s', framestatename);
         %         vidFrame = insertText(vidFrame,[x1,y1+60],str,...
         %             'FontSize',60,'Font', 'Arial', 'BoxColor', boxcolor,  ...
         %             'BoxOpacity',0.4,'TextColor','white');
         
+<<<<<<< Updated upstream
         %add frame counter & drug
+=======
+        %add frame counter
+>>>>>>> Stashed changes
         %the counters are: trialnum localframenum/numframes
         %cumframenum/totalframes
         vidFrame = insertText(vidFrame,[10,900],...
             sprintf('trial%d %d/%d %d/%d',i, f,numframes, cumframeidx, length(Z)),...
             'FontSize',48,'Font', 'Arial', 'BoxColor', 'g',  ...
             'BoxOpacity',0,'TextColor','white');
+<<<<<<< Updated upstream
         vidFrame = insertText(vidFrame,[10,950],drug, ...
             'FontSize',48,'Font', 'Arial', 'BoxColor', 'g',  ...
             'BoxOpacity',0,'TextColor','white');
         vidFrame = insertText(vidFrame,[10,1025],...
             sprintf('%s',movie_filename),...
             'FontSize',36,'Font', 'Arial', 'BoxColor', 'g',  ...
+=======
+        vidFrame = insertText(vidFrame,[10,1025],...
+            sprintf('%s',movie_filename),...
+            'FontSize',24,'Font', 'Arial', 'BoxColor', 'g',  ...
+>>>>>>> Stashed changes
             'BoxOpacity',0,'TextColor','white');
         
         writeVideo(vout,vidFrame)
@@ -225,7 +303,12 @@ for i=1:length(Groupdata);
     
     
 end
+<<<<<<< Updated upstream
 
 close(vout)
 cd    '/Users/mikewehr/Documents/Analysis/PreyCapture data'
+=======
+cd(outputdir)
+close(vout)
+>>>>>>> Stashed changes
 beep
