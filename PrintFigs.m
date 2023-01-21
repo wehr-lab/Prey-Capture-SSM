a7f3d90681875c018eb66dc1c2de325e84e78f5d
mikewehr <wehr@uoregon.edu>
2023-01-20 16:04:03 -0800

    Update PrintFigs.m

--- PrintFigs.m
+++ PrintFigs.m
@@ -5,17 +5,24 @@ psfilename='figs.ps';
 cd(outputdir) %absolute
 figs=findobj('type', 'figure');
 figs=fliplr(figs');
+fprintf('\nfigs: ');
+fprintf(' %d', figs.Number);
+
 numfigs=length(figs);
 %this may seem like crazy overkill but it keeps crashing when trying to
 %print a figure that is already closed, still cannot figure out why
 for f=figs
 
-try    figure(f), end
-    drawnow
-    pause(.1)
-    fprintf('\nprinting figure %d/%d', f.Number, numfigs)
-    print('-dpsc2', '-append', '-bestfit', psfilename)
-    pause(.1)
-    fprintf('\n closing figure %d/%d', f.Number, numfigs)
-    close
+    try
+        figure(f)
+        drawnow
+        pause(.1)
+        fprintf('\nprinting figure %d/%d', f.Number, numfigs)
+        print('-dpsc2', '-append', '-bestfit', psfilename)
+        pause(.1)
+        fprintf('\n closing figure %d/%d', f.Number, numfigs)
+        close
+    catch
+        fprintf('\nfailed to print figure')
+    end
 end
