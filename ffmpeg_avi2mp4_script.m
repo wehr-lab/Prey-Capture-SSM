6ee58a0da85f2b2fd22774e2755d572cac658104
Mike Wehr <wehr@uoregon.edu>
2023-02-08 09:40:10 -0800

    Update ffmpeg_avi2mp4_script.m

--- ffmpeg_avi2mp4_script.m
+++ ffmpeg_avi2mp4_script.m
@@ -13,28 +13,30 @@ movieroot='/home/wehr/wehrlab/save_OEablationSocial/param_search'
 cd(movieroot)
 d=dir('state-epoch-clips-2023*');
 for i=1:length(d)
-d2=dir(sprintf('%s/ssm_state_vid-comp*', d(i).name));
-for j=1:length(d2)
-    
-moviefilename=d(2).name;
-fps=30; %desired fps
-%targetdir='"/Users/wehr/Documents/Presentations/UCSC 2022/videos/"';
-newmoviefilename=replace(moviefilename, '.mp4', sprintf('-%dfps.mp4', fps));
-x5moviefilename=replace(moviefilename, '.mp4', sprintf('-%dfps5x.mp4', fps));
-
-%ffmpeg
-str=sprintf('!/usr/local/bin/ffmpeg  -i %s -r %d %s', moviefilename, fps, newmoviefilename);
-% eval(str)
-fprintf('\n%s', str)
-
-str=sprintf('!/usr/local/bin/ffmpeg  -i %s -filter:v "setpts=0.2*PTS" %s', newmoviefilename, x5moviefilename);
-%eval(str)
-
-%move to new target dir
-% str=sprintf('!mv %s %s', newmoviefilename, targetdir);
-% eval(str)
-% str=sprintf('!mv %s %s', x5moviefilename, targetdir);
-% eval(str)
-% 
-% cd(erase(targetdir, '"'))
-
+    d2=dir(sprintf('%s/ssm_state_vid-comp*', d(i).name));
+    for j=1:length(d2)
+        
+        moviefilename=d(2).name;
+        fps=30; %desired fps
+        %targetdir='"/Users/wehr/Documents/Presentations/UCSC 2022/videos/"';
+        newmoviefilename=replace(moviefilename, '.mp4', sprintf('-%dfps.mp4', fps));
+        x5moviefilename=replace(moviefilename, '.mp4', sprintf('-%dfps5x.mp4', fps));
+        
+        %ffmpeg
+        str=sprintf('!/usr/local/bin/ffmpeg  -i %s -r %d %s', moviefilename, fps, newmoviefilename);
+        % eval(str)
+        fprintf('\n%s', str)
+        
+        str=sprintf('!/usr/local/bin/ffmpeg  -i %s -filter:v "setpts=0.2*PTS" %s', newmoviefilename, x5moviefilename);
+        %eval(str)
+        
+        %move to new target dir
+        % str=sprintf('!mv %s %s', newmoviefilename, targetdir);
+        % eval(str)
+        % str=sprintf('!mv %s %s', x5moviefilename, targetdir);
+        % eval(str)
+        %
+        % cd(erase(targetdir, '"'))
+        
+    end
+end
