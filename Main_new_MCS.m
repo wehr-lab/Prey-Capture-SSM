4545844ed65086cc10171ec6905f8817b575e3bd
mshallow <c.shallow@wustl.edu>
2023-05-26 12:11:44 -0700

    changes to main file for MCS data format

---
+++ Main_new_MCS.m
@@ -0,0 +1,112 @@
+%Main workflow for running SSM on prey capture data
+% Oct 2020 mw
+%updated for new file hierarchy Feb 2021 (original version is now Main_old
+
+% machine-specific settings
+switch char(java.net.InetAddress.getLocalHost.getHostName)
+    case 'Transporter.local'
+        % path and filename(s) of group data with DLC tracks
+        groupdatadir='/Users/mikewehr/Documents/Analysis/PreyCapture data';
+        groupdatafilename{1}='preycapture_groupdata_saline_1.mat'; %cell array of filenames
+        %root directory for source movies
+        local_movie_root='/Users/mikewehr/Documents/Analysis/PreyCapture data/';
+        %output directory for results and generated video clips
+        outputroot='/Users/mikewehr/Documents/Analysis/PreyCapture data';
+        %local path to python3
+        pypath='/usr/local/bin/python3';
+    case     'dyn-184-171-85-20.uoregon.edu'
+  % path and filename(s) of group data with DLC tracks
+        groupdatadir='/Volumes/Lennon/Documents/Analysis/PreyCapture data';
+        groupdatafilename{1}='preycapture_groupdata_saline_1.mat'; %cell array of filenames
+        %root directory for source movies
+        local_movie_root='/Volumes/Lennon/Prey Capture/Netanyas Source Videos/';
+        %output directory for results and generated video clips
+        outputroot='/Volumes/Lennon/Prey Capture';
+        %local path to python3
+        pypath='/usr/local/bin/python3';
+        activate_venv_cmd='source ~/virtualenvironment/ssmenv/bin/activate'
+    case 'DESKTOP-4NTC7RV'
+        % path and filename(s) of group data with DLC tracks
+        groupdatadir='C:\Users\Kat\Resilio Sync\Prey Capture\matlab code July 2020';
+        groupdatafilename{1}='preycapture_groupdata_saline_1.mat'; 
+        local_movie_root='C:\Users\Kat\Resilio Sync\Prey Capture\Netanyas Source Videos\';
+        %output directory for results and generated video clips
+        outputroot='C:\Users\Kat\Resilio Sync\Prey Capture';
+        %local path to python3
+        pypath='C:\Users\Kat\python'
+        %'/usr/local/bin/anaconda3.exe';
+     case 'dyn-10-109-115-136.wless.uoregon.edu'
+        % path and filename(s) of group data with DLC tracks
+        groupdatadir='/Users/mollyshallow/Desktop/Wehr_Lab/PreyCaptureHMM';
+        groupdatafilename{1}='preycapture_groupdata_saline_1.mat'; %cell array of filenames
+        %root directory for source movies
+        local_movie_root='mount to rig2';
+        %output directory for results and generated video clips
+        outputroot='/Users/mollyshallow/Desktop/Wehr_Lab/PreyCaptureHMM';
+        %local path to python3
+        pypath='/Users/mollyshallow/opt/anaconda3/bin/python3';
+    otherwise
+        fprintf('don''t recognize computer %s', java.net.InetAddress.getLocalHost.getHostName)
+        return
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% let's try to keep everything else machine-independent
+    
+%make a new output directory with today's date:
+outputdir=sprintf('%s%sstate_epoch_clips-%s',outputroot, filesep, datestr(today));
+%OR
+%use an existing output directory
+%outputdir=sprintf('%s%sstate_epoch_clips-%s',outputroot, filesep, datestr('10-10-2020'));
+
+cd(outputroot)
+mkdir(outputdir)
+cd(outputdir)
+% [DirL     ist, dirlistpath] = uigetfile('*.txt', 'select DirList of data directories to scan');
+% if isequal(DirList,0) || isequal(dirlistpath,0)
+%     fprintf('\ncancelled')
+%     return
+% end
+
+% DirList=    'processedvids_ephys.txt';
+% dirlistpath=    '/Volumes/wehrrig4.uoregon.edu/lab/djmaus/Data/Molly/';
+DirList=    'combined_05_12_21.txt';
+dirlistpath=    '/Volumes/wehrrig4.uoregon.edu/lab/djmaus/Data/Kip/';
+
+
+ConvertGeometryToObservations(DirList, dirlistpath, outputdir)
+
+[repositorydir,~,~]=fileparts(which(mfilename));
+ssmfilename=fullfile(repositorydir, 'ssm_preycap_posterior.py');
+
+%run hmm python code in a system shell
+cmdstr=sprintf('%s %s', pypath, ssmfilename);
+system(activate_venv_cmd);
+system(cmdstr);
+
+% since we decimated ConvertGeometryToObservations, now we un-decimate the Z returned by the hmm
+fprintf('\nundecimating...')
+load ssm_posterior_probs
+td=load('training_data');
+post_probs=Ps{1};
+j=0;
+for i=1:length(Z)
+    post_probs_undec(j+1:j+td.decimate_factor,:)=repmat(post_probs(i,:),td.decimate_factor,1);
+    Zundec(j+1:j+td.decimate_factor)=Z(i);
+    j=j+td.decimate_factor;
+end
+Zundec=Zundec(1:length(td.undecX));
+post_probs_undec=post_probs_undec(1:length(td.undecX),:);
+save ssm_posterior_probs Ps  TM Z Zundec post_probs post_probs_undec hmm_lls num_states obs_dim run_from run_on ...
+    transitions kappa AR_lags observation_class
+
+PlotPosteriorProbs(outputdir)
+
+PruneTPM(outputdir)
+PlotEPM(outputdir)
+GenerateStateEpochClips(outputdir)
+TileVideoClips(outputdir)
+% LabelMovieStates(outputdir)
+%PlotStatePSTH(outputdir)
+
---
+++ ssm_preycap_posterior_mcs.py
@@ -0,0 +1,39 @@
+# loads training data from mat-file, saves posterior probabilities into ssm_posterior_probs.mat
+
+import numpy
+import ssm
+from scipy import io
+import time
+import os
+
+# Build an HMM instance and set parameters
+#np.random.seed(1)
+num_states = 10    # number of discrete states
+observation_class = 'autoregressive'
+obs_dim = 9       # dimensionality of observation
+transitions = 'sticky'
+kappa = 0
+AR_lags =  10
+hmm = ssm.HMM(num_states, obs_dim,
+              observations=observation_class, observation_kwargs={'lags':AR_lags},
+              transitions=transitions, transition_kwargs={'kappa': kappa})
+print([num_states, kappa, AR_lags])
+
+#load data using loadmat
+mat=io.loadmat('training_data.mat')
+#mat=io.loadmat('C:/Users/Kat/Resilio Sync/Prey Capture/state_epoch_clips-06-Jan-2021/training_data.mat')
+X = mat['X']
+
+#fit hmm to data
+N_iters=20
+#N_iters=10
+hmm_lls = hmm.fit(X, method="em", num_iters=N_iters)
+Z = hmm.most_likely_states(X)
+Ps = hmm.expected_states(X)
+TM = hmm.transitions.transition_matrix
+run_on = time.asctime( time.localtime(time.time()) )
+run_from = os.getcwd()
+
+#save output files using savemat
+mdict={'Z': Z, 'Ps': Ps, 'num_states': num_states, 'obs_dim': obs_dim, 'transitions': transitions, 'kappa': kappa, 'AR_lags': AR_lags, 'hmm_lls':hmm_lls, 'TM':TM, 'observation_class': observation_class, 'run_on':run_on, 'run_from':run_from}
+io.savemat('ssm_posterior_probs.mat', mdict)
